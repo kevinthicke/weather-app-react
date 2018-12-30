75062c7d7a4555583d24a5326986ee5a85fec2cb
Kevin Cabrera <keviincabrera@gmail.com>
2018-12-30 14:44:27 +0000

    iniciando diseño responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import  { Grid, Row, Col } from 'react-flexbox-grid';
 import * as styles from './styles.css';
 import LocationList from './components/LocationList';
+import ExtendedWeatherLocation from './components/ExtendedWeatherLocation';
 
 const aLocations = ["Bogota", "Lima", "Medellín", "Tenerife", "Barcelona", "Roma", "Cali", "London"];
 
 export default class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <LocationList aLocations={aLocations}/>
-      </div>
+      <Grid className={styles.App}>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList aLocations={aLocations}/> 
+          </Col>
+          <Col xs={12} md={6}>
+            <ExtendedWeatherLocation/>
+          </Col>
+        </Row>
+        
+      </Grid>
       );
   }
 }
---
+++ src/components/ExtendedWeatherLocation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as styles from './styles.css';
+
+const ExtendedWeatherLocation = () => {
+    return (
+        <div className={styles.ExtendedWeatherLocation}> Extended Weather Location</div>
+    )
+}
+
+export default ExtendedWeatherLocation;
